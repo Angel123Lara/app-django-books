d4ddf3ea5d34923ac85f4302a4759eb04ef477ee
Angel123Lara <adjls.2395@gmail.com>
2020-07-15 22:01:09 -0500

    App-with-Django

---
+++ bedu/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+from books.views import home_books
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_books, name='index'),
+]
+
+# Add static and media urls
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Book
+# Create your views here.
+def home_books(request):
+    books = Book.objects.all()
+    return render(request, 'home_books.html',{
+        'books': books
+    })
